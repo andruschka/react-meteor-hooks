c0ad44f29f93210be0062fae1c3a3f058181e334
Max <max.finke@gmail.com>
2019-05-24 04:10:14 +0000

    Use useReducer to avoid unnecessary rerenders
    Thanks @JoschuaSchneider

--- hooks/useMethod.js
+++ hooks/useMethod.js
@@ -1,22 +1,50 @@
-import { useState } from 'react'
+import { useReducer } from 'react'
 
 export default function useMethod(methodName, { transform } = {}) {
-  const [isLoading, setIsLoading] = useState(false)
-  const [data, setData] = useState(null)
-  const [error, setError] = useState(null)
+  const [{ isLoading, error, data }, dispatch] = useReducer(
+    (state, action) => {
+      switch (action.type) {
+        case 'loading':
+          return { ...state, isLoading: true }
+        case 'success':
+          return {
+            ...state,
+            isLoading: false,
+            error: null,
+            data: action.payload
+          }
+        case 'failure':
+          return {
+            ...state,
+            isLoading: false,
+            error: action.payload,
+            data: null
+          }
+        default:
+          return state
+      }
+    },
+    {
+      isLoading: false,
+      data: null,
+      error: null
+    }
+  )
 
   const call = (...args) => {
-    setIsLoading(true)
+    dispatch({ type: 'loading' })
     return new Promise((resolve, reject) => {
       Meteor.call(methodName, ...args, (err, result) => {
         if (err) {
-          setError(err)
+          dispatch({ type: 'failure', payload: err })
           reject(err)
         } else {
-          setData(transform ? transform(result) : result)
+          dispatch({
+            type: 'success',
+            payload: transform ? transform(result) : result
+          })
           resolve(result)
         }
-        setIsLoading(false)
       })
     })
   }
